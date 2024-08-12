be9c8ae91932724312b32b60fdbbde1ea2b40733
tarique312 <tariquemdofficial@gmail.com>
2024-08-12 20:31:07 +0530

    sixth try changed PORT

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser");
 const { getStoredItems } = require("./data/data");
 
 const app = express();
-const PORT = process.env.PORT || 8880;
+const PORT = process.env.PORT || 10000;
 
 app.use(bodyParser.json());
 
